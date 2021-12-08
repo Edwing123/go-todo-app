c7527b445d995ee43c8f52c620cfea9c8514f9d9
Edwing123 <egarciavalle2014@gmail.com>
2021-12-08 16:49:32 -0600

    chore: set up dependencies and server

---
+++ cmd/web/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"crypto/tls"
+	"net/http"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	// Get command-line flags.
+	config, err := getConfigFlags()
+	if err != nil {
+		errLogger.Fatalln(err)
+	}
+
+	// Get templates map.
+	templates, err := createTemplatesMap("ui/html")
+	if err != nil {
+		errLogger.Fatalln(err)
+	}
+
+	// Get database connections pool.
+	db, err := createDatabasePool(config.dsn)
+	if err != nil {
+		errLogger.Fatalln(err)
+	}
+	defer db.Close()
+
+	// Setup application dependencies.
+	app := &application{
+		db:         db,
+		templates:  templates,
+		infoLogger: infoLogger,
+		errLogger:  errLogger,
+	}
+
+	// TSL config for the server.
+	tlsConfig := &tls.Config{
+		CurvePreferences: []tls.CurveID{tls.CurveP256, tls.X25519},
+	}
+
+	// Setup server.
+	server := &http.Server{
+		Addr:      config.addr,
+		Handler:   app.getRouter(),
+		ErrorLog:  errLogger,
+		TLSConfig: tlsConfig,
+
+		// Server connection timeout settings.
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  time.Second * 5,
+		WriteTimeout: time.Second * 10,
+	}
+
+	// Start server using tls certificate and private key.
+	infoLogger.Printf("Listening on %s\n", config.addr)
+	errLogger.Fatalln(server.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem"))
+}
