6ed992c87c6d789c76e8768d23143afc99ae10f1
Edwing123 <egarciavalle2014@gmail.com>
2021-12-08 15:25:55 -0600

    chore: create function to get db connections pool

---
+++ cmd/web/db.go
@@ -0,0 +1,17 @@
+package main
+
+import "database/sql"
+
+// Create database connections pool using the MySQL driver
+func createDatabasePool(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
