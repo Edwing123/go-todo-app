7062278cc4a85b53ee656c17c1ce9ea2594b3f95
Edwing123 <egarciavalle2014@gmail.com>
2021-12-09 10:12:08 -0600

    chore: add notFound helper method

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -63,3 +63,7 @@ func (app *application) serverError(w http.ResponseWriter, err error) {
 func (app *application) clientError(w http.ResponseWriter, status int) {
 	http.Error(w, http.StatusText(status), status)
 }
+
+func (app *application) notFound(w http.ResponseWriter, r *http.Request) {
+	http.NotFound(w, r)
+}
