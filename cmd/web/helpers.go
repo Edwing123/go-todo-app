ee71055aed44dc56623c7cf1e73edc195ebd9696
Edwing123 <egarciavalle2014@gmail.com>
2021-12-09 08:34:37 -0600

    chore: add helper methods
    Instead of managing the execution of templates and server errors,
    which involves a few steps, I decided to create some helper
    methods to abstract these repetitive tasks.
    
    For now I'm including:
        - template render method
        - serverError and clientError methods

---
+++ cmd/web/helpers.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+	"runtime/debug"
+)
+
+func (app *application) render(w http.ResponseWriter, r *http.Request, name string, data *viewData) {
+	// Get template set from templates map.
+	// The name is suffixed with the extension "go.html",
+	// this way I don't have to pass the whole name with the extesion.
+	ts, ok := app.templates[name+".go.html"]
+
+	// Check if the template set exists.
+	if !ok {
+		app.serverError(w, fmt.Errorf("the template %q does not exist", name))
+		return
+	}
+
+	// Executing a template could produce an error, so instead of writing
+	// to the response body directly, first write to a buffer.
+	buffer := new(bytes.Buffer)
+
+	err := ts.ExecuteTemplate(buffer, name, data)
+
+	// Check for any error while executing the template.
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	// If everything is fine write the rendered template
+	// to the response body.
+	buffer.WriteTo(w)
+}
+
+// The serverError helper writes an error message and stack trace to the errLogger output,
+// then sends a generic 500 Internal Server Error response to the user.
+func (app *application) serverError(w http.ResponseWriter, err error) {
+	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
+	app.errLogger.Output(2, trace)
+
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+}
+
+// Sends generic client-related error messages.
+func (app *application) clientError(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
+}
